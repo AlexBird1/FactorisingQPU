067af48d22a732788f86d05ff8da723210c88a32
AlexBird1 <53532646+AlexBird1@users.noreply.github.com>
2020-05-06 15:42:02 +0100

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,12 @@
+def to_base_ten(sample):
+    a = b = 0
+    
+    a_vars = ['a0', 'a1', 'a2']
+    b_vars = ['b0', 'b1', 'b2']
+    
+    for lbl in reversed(a_vars):
+        a = (a << 1) | sample[lbl]
+    for lbl in reversed(b_vars):
+        b = (b << 1) | sample[lbl] 
+        
+    return a,b
